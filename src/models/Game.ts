95f4e060007bd71b69be7a763fddf16ea2af509c
Hannes Ensbacher <hensbacher@gmail.com>
2025-01-17 15:59:18 +0100

    Adapted game model to include intensity.ts

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -1,4 +1,6 @@
-export interface Game {
+import { Intensity } from './intensity';
+
+export class Game {
     id: number;
     name: string;
     cover: string;
@@ -6,4 +8,27 @@ export interface Game {
     keywords: string[];
     platforms: string[];
     description?: string;
+    intensity?: Intensity;
+
+    constructor(data: any) {
+        this.id = data.id;
+        this.name = data.name;
+        this.cover = data.cover;
+        this.first_release_date = data.first_release_date;
+        this.keywords = data.keywords;
+        this.platforms = data.platforms;
+        this.description = data.description;
+        this.intensity = data.intensity;
+    }
+
+    static validateGame(game: any): boolean {
+        return (
+            typeof game.id === 'number' &&
+            typeof game.name === 'string' &&
+            typeof game.cover === 'string' &&
+            typeof game.first_release_date === 'string' &&
+            Array.isArray(game.keywords) &&
+            Array.isArray(game.platforms)
+        );
+    }
 }
